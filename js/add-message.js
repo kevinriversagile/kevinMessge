31f8e46b66a3052ea2bc6131c3e6f02a8768aa37
Kevin Mason <kevin.mason@acrobatiq.com>
2016-08-04 20:03:54 -0400

    first commit

--- js/add-message.js
+++ js/add-message.js
@@ -1,4 +1,38 @@
+var apiEndpointBase = 'http://code-school-comments-api.herokuapp.com/comments';
+
+
 function addMessage() {
-  // TODO: your solution goes here
-  window.location.href = '/';
+
+  var createdby = document.getElementById('myname').value;
+  var messageBox = document.getElementById('message').value;
+  var important = document.getElementById('important').checked;
+
+  var comments = {
+    comment: {
+      commentText: messageBox,
+      createdBy: createdby,
+      isImportant: important
+    }
+  }
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      window.location.href = 'index.html';
+    }
+  }
+  xhttp.open("POST", apiEndpointBase, true); 
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.send(JSON.stringify(comments));       
 }
+
+//function editMessage() {
+  //var editComments = {
+//     comment: {
+//       commentText: messageBox,
+//       isImportant: false
+//     }
+//   }
+// }
+//   var xhttp = new XMLHttpRequest
+
--- js/edit-message.js
+++ js/edit-message.js
@@ -1,10 +1,55 @@
+
+
 function getMessage() {
-  // TODO: your solution goes here
+
+    //var createdby = document.getElementById('myname').value;
+    //var messageBox = document.getElementById('message').value;
+    //var important = document.getElementById('important').checked;
+
+
+    var myMessages ={
+    
+    
+  }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        var thisComment =JSON.parse(xhttp.responseText);
+        document.getElementById('txtCreatedBy').value = thisComment.createdBy;
+        document.getElementById('txtComment').value = thisComment.commentText;
+        //document.getElementById('message').value = thisComment.id;
+        document.getElementById('important').checked = thisComment.isImportant;
+        console.log(xhttp.responseText);
+      }     
+    }
+    xhttp.open("GET", apiEndpointBase + '/' + messageId, true); 
+    xhttp.send();  
 }
 
+
 function editMessage() {
-  // TODO: your solution goes here
-  window.location.href = '/index.html';
+  var messageBox = document.getElementById('txtComment').value;
+  var important = document.getElementById('important').checked;
+ 
+
+  var edits = {
+    comment: {
+      commentText: messageBox,
+      isImportant: important
+    }
+  };
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      window.location.href = 'index.html';
+    }
+  }
+
+  xhttp.open("PUT", apiEndpointBase + '/' + messageId, true); 
+   xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(edits));   
+
+  return false;      
 }
 
 var messageId = getQueryStringValue('messageId');
@@ -13,19 +58,18 @@ if (messageId) {
   messageId = parseInt(messageId, 10);
 } else {
   var result = confirm("Sorry pal, you can't edit a message unless it's got an id!");
-
+}
   // result true means they clicked OK
-  if (result === true) {
-    window.location.href = '/';
-  } else {
-    window.location.href = '/';
-  }
+  // if (result === true) {
+  //   window.location.href = '/';
+  // } else {
+  //   window.location.href = '/';
+   //}
+
+  getMessage();
 
   // PSST! Hey you! Yeah, you intrepid student!
   // We're sending them back to the index no matter what they choose.
   // In that case, I didn't have to use an if/else block.
   // But, you might want to use a confirm box somewhere else in your code
   // and I thought this might be handy. HINT HINT!
-}
-
-getMessage();
